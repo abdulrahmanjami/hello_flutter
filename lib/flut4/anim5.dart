2904aa255a4f44845f35f39c6f2332a869e79b4d
Ehsan Hasin <abdulrahmanjami44@gmail.com>
2023-12-17 12:18:24 +0430

    Animation Day #3 anim5.dart AnimatedIcon widget

---
+++ lib/flut4/anim5.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
+
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this, duration: Duration(seconds: 1));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedIcon(icon: AnimatedIcons.close_menu, progress: controller, size: 70, color: Colors.lightGreenAccent,),
+              ElevatedButton(onPressed: (){
+                controller.forward();
+              }, child: Text("Forward Animation")),
+              ElevatedButton(onPressed: (){
+                controller.reverse();
+              }, child: Text("Reverse Animation")),
+              ElevatedButton(onPressed: (){
+                controller.repeat(reverse: true);
+              }, child: Text("Animation")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
